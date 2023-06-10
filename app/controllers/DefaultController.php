57332283dc5e4105ddd5e70a468c2dc751877d21
Edivaldo Reis <efrj@hotmail.com>
2023-06-09 22:01:19 -0300

    Updated Default Controller

--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -4,13 +4,7 @@ namespace Controllers;
 use Controllers\Controller;
 
 class DefaultController extends Controller
-{
-    public function __construct()
-    {
-        parent::__construct();
-        $this->index();
-    }
-    
+{   
     public function index()
     {
         $this->view->display('default');
