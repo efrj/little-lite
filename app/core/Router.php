b56cbb96e4592d37eec7f962c4c8b9fb08dd1128
Edivaldo Reis <efrj@hotmail.com>
2023-09-01 09:43:21 -0300

    Improve error handling in Router class
    Enhanced the Router class to include better error handling using try/catch and exceptions. Now, when a controller or action is not found during routing, meaningful exceptions are thrown with descriptive error messages. This improves the code's robustness and makes it easier to troubleshoot routing issues.
    
    Changes made:
    - Implemented try/catch and exceptions for error handling
    - Throwing exceptions with informative error messages
    - Enhanced code reliability and debugging capabilities
    
    This commit enhances the error-handling capabilities of the Router class.

--- app/core/Router.php
+++ app/core/Router.php
@@ -8,41 +8,45 @@ class Router
     
     public function route($url)
     {
-        $queryArray = [];
+        try {
+            $queryArray = [];
 
-        if (!empty($url) && $url !== '/') {
-            $queryArray = explode('/', $url);
-        }
+            if (!empty($url) && $url !== '/') {
+                $queryArray = explode('/', $url);
+            }
 
-        $controllerName = 'DefaultController';
-        $action = 'index';
+            $controllerName = 'DefaultController';
+            $action = 'index';
 
-        if (!empty($queryArray[1])) {
-            $controllerName = ucfirst($queryArray[1]) . 'Controller';
-        }
+            if (!empty($queryArray[1])) {
+                $controllerName = ucfirst($queryArray[1]) . 'Controller';
+            }
 
-        if (!empty($queryArray[2])) {
-            $action = $queryArray[2];
-        }
+            if (!empty($queryArray[2])) {
+                $action = $queryArray[2];
+            }
 
-        $controllerClass = $this->controllerNamespace . $controllerName;
+            $controllerClass = $this->controllerNamespace . $controllerName;
 
-        if (class_exists($controllerClass)) {
+            if (!class_exists($controllerClass)) {
+                throw new \Exception("Controller not found: $controllerClass");
+            }
+
             $controller = new $controllerClass();
 
-            if (method_exists($controller, $action)) {
-                if (!empty($queryArray[3])) {
-                    $controller->$action($queryArray[3]);
-                } else {
-                    $controller->$action();
-                }
-            } else {
-                header("HTTP/1.1 404 Not Found");
-                echo "Action not found.";
+            if (!method_exists($controller, $action)) {
+                throw new \Exception("Action not found: $action");
             }
-        } else {
+
+            if (!empty($queryArray[3])) {
+                $controller->$action($queryArray[3]);
+            } else {
+                $controller->$action();
+            }
+        } catch (\Exception $e) {
+            // Handle exceptions here
             header("HTTP/1.1 404 Not Found");
-            echo "Controller not found.";
+            echo $e->getMessage();
         }
     }
 }
