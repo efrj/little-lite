3a9438d90f4a98eb8f4cae0d1c0de65c08798ffa
Edivaldo Reis <efrj@hotmail.com>
2023-09-04 10:28:26 -0300

    Added Monolog to View class

--- app/core/View.php
+++ app/core/View.php
@@ -1,11 +1,23 @@
 <?php
+
 namespace Core;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 class View
 {
     public $data = array();
+    private $logger; // Adicione uma propriedade para o Logger
+    private $logPath = __DIR__ . '/../logs/app.log';
+
+    public function __construct()
+    {
+        $this->logger = new Logger('view_logger');
+        $this->logger->pushHandler(new StreamHandler($this->logPath, Logger::DEBUG));
+    }
     
-    public function assign($varname,$vardata)
+    public function assign($varname, $vardata)
     {
         $this->data[$varname] = $vardata;
     }
@@ -13,6 +25,17 @@ class View
     public function display($filename)
     {
         extract($this->data);
-        include(__DIR__ . '/../views/' . $filename . '.php');
+
+        try {
+            $viewFilePath = __DIR__ . '/../views/' . $filename . '.php';
+            if (!file_exists($viewFilePath)) {
+                throw new \Exception("View file not found: $viewFilePath");
+            }
+
+            include($viewFilePath);
+        } catch (\Exception $e) {
+            $errorMessage = "Error displaying view: $filename - " . $e->getMessage();
+            $this->logger->error($errorMessage);
+        }
     }
 }
